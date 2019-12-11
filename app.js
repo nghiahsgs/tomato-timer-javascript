ff6855f3bfb34f2b9ac09dcccd8eb9f3b196f926
nghiahsgs <nghiahsgs@gmail.com>
2019-12-12 06:19:11 +0700

    make it better

---
+++ app.js
@@ -0,0 +1,54 @@
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+var timeinterval;
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            document.getElementById('audioEndtime').play();
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    timeinterval = setInterval(updateClock, 1000);
+}
+
+function startWork() {
+    clearInterval(timeinterval);
+    $('#status').text('Time to work')
+    document.getElementById('audioWork').play();
+    var deadline = new Date(Date.parse(new Date()) + 25 * 60 * 1000);
+    initializeClock('clockdiv', deadline);
+}
+
+function startRelax() {
+    clearInterval(timeinterval);
+    $('#status').text('Time to realax')
+    document.getElementById('audioRelax').play();
+    var deadline = new Date(Date.parse(new Date()) + 5 * 60 * 1000);
+    initializeClock('clockdiv', deadline);
+}
+// $(document).ready(function () {
+    
+// });
